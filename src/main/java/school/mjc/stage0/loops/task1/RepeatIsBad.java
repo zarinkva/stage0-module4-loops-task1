d488da0a90ace4fe3b1ef0153a24858161a142bf
zarina <zarinakamalova117@gmail.com>
2023-03-13 16:41:35 +0600

    done

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -3,7 +3,17 @@ package school.mjc.stage0.loops.task1;
 public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
-    public void repeatIsBad(){
+    public void repeatIsBad(int count){
+        if(count <= 0) {
+            return;
+        }
+        System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        repeatIsBad(count - 1);
 
+    }
+
+    public static void main(String[] args) {
+        RepeatIsBad mainObj = new RepeatIsBad();
+        mainObj.repeatIsBad(20);
     }
 }
